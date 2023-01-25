ba9881f34b6fe2fd0685183ee0fff831b6affd9d
moralesvictorr <vmmorales@unicauca.edu.co>
2023-01-25 18:39:01 -0500

    feat: Registros solved & added

--- src/Containers/Form.jsx
+++ src/Containers/Form.jsx
@@ -3,9 +3,7 @@ import { set, useForm } from 'react-hook-form'
 
 const Form = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  //const onSubmit = data; 
   const [entradas, setEntradas] = useState([]);
-  const [mostrar, setMostrar] = useState("")
   const procesarForm = (data, e) => {
     console.log(data);
     setEntradas([
@@ -18,24 +16,12 @@ const Form = () => {
     <Fragment>
     <form onSubmit={handleSubmit(procesarForm)}>
       <div className='container'>
-        {/* <input
-    
-    id="accountReference"
-    
-    className={
-      errors?.accountReference?.message ? styles["error"] : "no-error"
-    }
-    
-    {...register("accountReference", {
-      
-      required: "Account reference is required",
-      
-    })} 
-  /> */}
-        <input className='inputs' name='firstname' placeholder='First Name' {...register("firstName", { required: "firstname cannot be empty" })} />
-        <input className='inputs' name='lastname' placeholder='Last Name' required {...register("lastName")} />
-        <input className='inputs' name='email' placeholder='Email Address' type="email" required {...register("email")} />
-        <input className='inputs' name='password' placeholder='Password' type="password" required {...register("password")} />
+
+        <input className='inputs'  placeholder='First Name' {...register("firstName", { required:true})} />
+        {errors.firstName?.type === 'required' && <span role="alert">First name is required</span>}
+        <input className='inputs'  placeholder='Last Name' required {...register("lastName")} />
+        <input className='inputs'  placeholder='Email Address' type="email" required {...register("email")}  />
+        <input className='inputs'  placeholder='Password' type="password" required {...register("password")} />
         <button type='submit' className='boton'>CLAIM YOUR FREE TRIAL</button>
 
         <p id="terminos1">By clicking the button you are agreeing to our <strong id="terminos2">Terms and Services</strong></p>
@@ -43,15 +29,14 @@ const Form = () => {
       </div>
       
     </form>
-    <ul className='prueba'>
-      <h1>prueba</h1>
-      {console.log(entradas)}
+    <ul className='registros'>
+      <h1>Registros</h1>
       {
-        entradas.map((item, index) => {
-          <li key={index}>
-            {item.firstname} - {item.lastname} - {item.email} - {item.password}
+        entradas.map((item) => 
+          <li>
+            {item.firstName} - {item.lastName} - {item.email} - {item.password}
           </li>
-        })
+        )
       }
     </ul>
   </Fragment>
