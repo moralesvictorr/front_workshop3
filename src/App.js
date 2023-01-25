3c7f236610984fe1cb5bc5b39eac0aa3e3abc669
juandrojasr <juandrojasr97@gmail.com>
2023-01-24 19:49:55 -0500

    feat: styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Form from './Containers/Form';
 import BannerPromo from './Components/BannerPromo';
 import Description from './Components/Description';
 import "./Styles/app.scss"
+import "./Styles/descrption.scss"
+import "./Styles/form.scss"
+
 
 function App() {
   return (
--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+
 const Description = () => {
   return (
-    <div>
+    <div className='Complemento'>
         <h1>Learn to code by watching others</h1>
         <p>See how experienced developers solve problemas in real-time. Whatching scripted tutorials is great, but understanding how developers think is invaluable.</p>
     </div>
