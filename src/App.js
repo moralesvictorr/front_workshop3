7df5c87b21286fe591ae0f32012b210cd64c1765
yurledyspuche25 <89873596+yurledyspuche25@users.noreply.github.com>
2023-01-24 20:27:40 -0500

    feat: add bannerpromo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Form from './Containers/Form';
 import BannerPromo from './Components/BannerPromo';
 import Description from './Components/Description';
 import "./Styles/app.scss"
+import "./Styles/bannerPromo.scss"
 
 function App() {
   return (
