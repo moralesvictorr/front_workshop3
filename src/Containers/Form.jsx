125681b044f756d2ad9116dcf606af091c80e6db
yurledyspuche25 <89873596+yurledyspuche25@users.noreply.github.com>
2023-01-25 20:28:34 -0500

    feat: add style error-color

--- src/Containers/Form.jsx
+++ src/Containers/Form.jsx
@@ -19,16 +19,16 @@ const Form = () => {
       <div className='container'>
 
         <input className='inputs'  placeholder='First Name' {...register("firstName", { required:true})} />
-        {errors.firstName?.type === 'required' && <small role="alert">First name is required</small>}
+        {errors.firstName?.type === 'required' && <small className='error-color' role="alert">First name is required</small>}
 
         <input className='inputs'  placeholder='Last Name' {...register("lastName", { required:true})} />
-        {errors.lastName?.type === 'required' && <small role="alert">Last name is required</small>}
+        {errors.lastName?.type === 'required' && <small className='error-color' role="alert">Last name is required</small>}
 
         <input className='inputs'  placeholder='Email Address' type="email" {...register("email", { required:"Email Address is required"})} aria-invalid={errors.email ? "true" : "false"}   />
-        {errors.email && <small role="alert">{errors.email?.message}</small>}
+        {errors.email && <small className='error-color' role="alert">{errors.email?.message}</small>}
 
         <input className='inputs'  placeholder='Password' type="password" {...register("password", { required:true, pattern:/^(?=(?:.*\d))(?=.*[A-Z])(?=.*[a-z])(?=.*[.,*!?¿¡/#$%&+-_=<>;:])\S{8,64}$/})} />
-        {errors.password && <small role="alert">Password is required and must be more safe</small>}
+        {errors.password && <small className='error' role="alert">Password is required and must be more safe</small>}
 
         <button type='submit' className='boton'>CLAIM YOUR FREE TRIAL</button>
 
