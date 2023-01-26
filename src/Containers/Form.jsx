3d79c71aa52575780bf2d573ade7bd93dd14dc59
moralesvictorr <vmmorales@unicauca.edu.co>
2023-01-26 11:54:45 -0500

    feat: solving some style problems

--- src/Containers/Form.jsx
+++ src/Containers/Form.jsx
@@ -18,18 +18,26 @@ const Form = () => {
     <form onSubmit={handleSubmit(procesarForm)}>
       <div className='container'>
 
-        <input className='inputs'  placeholder='First Name' {...register("firstName", { required:true})} />
+        <input className={`form-control ${errors.firstName ? 'inputs invalid' : 'inputs'}`}  placeholder='First Name' {...register("firstName", { required:true})} />
+        <div className='error-container'>
         {errors.firstName?.type === 'required' && <small className='error-color' role="alert">First name is required</small>}
 
-        <input className='inputs'  placeholder='Last Name' {...register("lastName", { required:true})} />
+        </div>
+
+        <input className={`form-control ${errors.lastName ? 'inputs invalid' : 'inputs'}`}  placeholder='Last Name' {...register("lastName", { required:true})} />
+        <div className='error-container'>
         {errors.lastName?.type === 'required' && <small className='error-color' role="alert">Last name is required</small>}
+        </div>
 
-        <input className='inputs'  placeholder='Email Address' type="email" {...register("email", { required:"Email Address is required"})} aria-invalid={errors.email ? "true" : "false"}   />
+        <input className={`form-control ${errors.email ? 'inputs invalid' : 'inputs'}`}  placeholder='Email Address' type="email" {...register("email", { required:"Email Address is required"})} aria-invalid={errors.email ? "true" : "false"}   />
+        <div className='error-container'>
         {errors.email && <small className='error-color' role="alert">{errors.email?.message}</small>}
+        </div>
 
-        <input className='inputs'  placeholder='Password' type="password" {...register("password", { required:true, pattern:/^(?=(?:.*\d))(?=.*[A-Z])(?=.*[a-z])(?=.*[.,*!?¿¡/#$%&+-_=<>;:])\S{8,64}$/})} />
-        {errors.password && <small className='error' role="alert">Password is required and must be more safe</small>}
-
+        <input className={`form-control ${errors.password ? 'inputs invalid' : 'inputs'}`}  placeholder='Password' type="password" {...register("password", { required:true, pattern:/^(?=(?:.*\d))(?=.*[A-Z])(?=.*[a-z])\S{8,64}$/})} />
+        <div className='error-container'>     
+        {errors.password && <small className='error-color' role="alert">Password is required and must be safe</small>}
+        </div>
         <button type='submit' className='boton'>CLAIM YOUR FREE TRIAL</button>
 
         <p id="terminos1">By clicking the button you are agreeing to our <strong id="terminos2">Terms and Services</strong></p>
